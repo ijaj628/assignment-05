e33c309b37162d4f4d3f82edf405dfae0112fb83
ijaj628 <rijajur019@gmail.com>
2024-09-23 19:35:57 +0600

    home and blog section connect

---
+++ js/blog.js
@@ -0,0 +1,6 @@
+document.getElementById('btn-home').addEventListener('click', function(event){
+     event.preventDefault();
+     if(this.click){
+        window.location.href = '/index.html';
+     }
+})
---
+++ js/home.js
@@ -0,0 +1,6 @@
+document.getElementById('btn-blog').addEventListener('click', function(event){
+    event.preventDefault();
+    if(this.click){
+       window.location.href = '/blog.html';
+    }
+})
